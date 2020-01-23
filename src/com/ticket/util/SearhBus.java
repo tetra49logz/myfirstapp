1da2f73195bac551cee3b30af1bb71f86ee7629d
tetra49 <logesh1996cool@gmail.com>
2020-01-23 20:20:01 +0530

    Servlet

--- src/com/ticket/util/SearhBus.java
+++ src/com/ticket/util/SearhBus.java
@@ -1,17 +1,10 @@
 package com.ticket.util;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 public class SearhBus
 {
